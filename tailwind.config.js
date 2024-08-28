e99192430c214dfdd9078817413e2554ca52eefe
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-08-28 17:05:56 +0530

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //all the places were we are using tailwind
+    "./src/**/*.{html,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
