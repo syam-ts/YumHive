3cc3a95f776dbd80c44ce7357e43ddd2a6fac7f5
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-08-31 11:09:12 +0530

    card style

--- src/components/ResturarntCard.jsx
+++ src/components/ResturarntCard.jsx
@@ -5,10 +5,9 @@ const ResturarntCard = (res) => {
          <img className="card-img" src={res.image ? res.image : `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${res?.info?.cloudinaryImageId}`} /> 
       <h2> {res.name ? res.name : res?.info?.name } </h2>
       <h3>  {res.cuisine ? res.cuisine.join(', ') : res?.info?.cuisines?.join(", ") } </h3>
-      <h4>  {res.rating ? res.rating  : res?.info?.avgRating } </h4>
-     
+      <h4>  {res.rating ? res.rating  : res?.info?.avgRating } </h4> 
     </div>
-  );
-};
+  )
+}
 
 export default ResturarntCard
