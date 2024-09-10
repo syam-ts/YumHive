431a55682115f08f632115e0acf2278d1a6fa4e3
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-09-10 14:38:24 +0530

    shimmer

--- dist/index.f482b72b.js
+++ dist/index.f482b72b.js
@@ -27698,9 +27698,9 @@ parcelHelpers.defineInteropFlag(exports);
 var _jsxDevRuntime = require("react/jsx-dev-runtime");
 const Shimmer = ()=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
-        className: "res",
+        className: "flex gap-12 flex-wrap px-44 mt-32",
         children: Array(10).fill("").map((e, index)=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
-                className: "shimmer-card"
+                className: "h-[250px] w-[350px] rounded-md bg-gray-200 "
             }, index, false, {
                 fileName: "src/components/Shimmer.jsx",
                 lineNumber: 9,
--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -2,11 +2,11 @@ const Shimmer = () => {
 
 
     return (
-      <div className="res">
+      <div className="flex gap-12 flex-wrap px-44 mt-32">
         {Array(10)
           .fill("")
           .map((e, index) => (
-            <div key={index} className="shimmer-card"></div>
+            <div key={index} className="h-[250px] w-[350px] rounded-md bg-gray-200 "></div>
           ))}
       </div>
     )
@@ -15,3 +15,8 @@ const Shimmer = () => {
 
   export default Shimmer
 
+
+
+   
+    
+     
